546dc6e159db24789b5c42a7ab9b79fda400b958
BenjaminDHorne <benjamindhorne314@gmail.com>
2017-07-06 16:15:42 -0400

    added start num

--- scraper_run.py
+++ scraper_run.py
@@ -6,10 +6,11 @@ import scrape_tools.reddit_post_scraper as post_scraper
 import scrape_tools.reddit_comment_scraper as comment_scraper
 
 number_posts_to_collect = 300000
+start_num = 0
 with open("subs_to_collect.txt") as subs:
     subs_to_collect = [l.strip() for l in subs]
 
 for sub_to_collect in subs_to_collect:
-    post_scraper.post_scraper(number_posts_to_collect, sub_to_collect)
+    post_scraper.post_scraper(number_posts_to_collect, start_num, sub_to_collect)
     post_file = sub_to_collect+"_reddit_posts.json"
     comment_scraper.comment_scraper(sub_to_collect, post_file)
