8d7b399d4217373bebac3465f3e682c50bc5d5e5
BenjaminDHorne <benjamindhorne314@gmail.com>
2017-07-18 08:56:21 -0400

    code for quick check on progress

---
+++ check.py
@@ -0,0 +1,6 @@
+c=0
+with open("Democrats_postandcomment_data.json") as data:
+    for line in data:
+        c+=1
+print "Comment Trees Collected", c
+print "Percentage out of total posts", float(c)/150000
