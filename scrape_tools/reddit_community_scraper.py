da71e7802a952bb4608b12492eb086446fc8f99e
Benjamin D Horne <benjamindhorne314@gmail.com>
2018-02-23 10:38:32 -0500

    Update reddit_community_scraper.py

--- scrape_tools/reddit_community_scraper.py
+++ scrape_tools/reddit_community_scraper.py
@@ -12,9 +12,9 @@ import json
 
 import praw
 
-reddit = praw.Reddit(user_agent='Post and Comment Extraction (by /u/BDHResearch)',
-                     client_id='4iq6UiKGMOE35w', client_secret="_YZhBJI7_2wjqTSHRZSBtOOY1QY",
-                     username='BDHResearch', password='Bdbomb777')
+reddit = praw.Reddit(user_agent='Post and Comment Extraction)',
+                     client_id='', client_secret="",
+                     username='', password='') #insert client information here
 
 def fix(text):
     text = text.decode("ascii", "ignore")
@@ -31,14 +31,10 @@ def get_posts_with_id_collected():
     with open("post_links_done.txt") as done:
         links_collected = [link for link in done]
     return links_collected
-	
-def get_date(submission):
-    time = submission.created
-    return time
 
 def scraper(sub, date_range, outfile, collect_posts=True, collect_cmts=True):
     FORMAT = "%(asctime)s %(levelname)s %(module)s %(lineno)d %(funcName)s:: %(message)s"
-    logging.basicConfig(filename='scraper.log', filemode='a', level=logging.DEBUG, format=FORMAT)
+    logging.basicConfig(filename=sub+'.log', filemode='a', level=logging.DEBUG, format=FORMAT)
     warnings.filterwarnings("ignore")
 
     #check internet connection before start
@@ -57,7 +53,7 @@ def scraper(sub, date_range, outfile, collect_posts=True, collect_cmts=True):
                     if collect_posts:
                         #submission data extraction
                         data_dict = {}
-                        sub_data = {"id": "t3_"+str(submission.id), "score": submission.score, "numcmts": submission.num_comments,"title":submission.title , "selftext":submission.selftext, "url":submission.url, "author":str(submission.author), "is_self":submission.is_self, "time":get_date(submission)} 
+                        sub_data = {"id": "t3_"+str(submission.id), "time:": submission.created_utc, "score": submission.score, "numcmts": submission.num_comments,"title":submission.title , "selftext":submission.selftext, "url":submission.url, "author":str(submission.author), "is_self":submission.is_self} 
                         data_dict['submission'] = sub_data
 
                     if collect_cmts:
@@ -65,7 +61,7 @@ def scraper(sub, date_range, outfile, collect_posts=True, collect_cmts=True):
                         cmts_data = {}
                         submission.comments.replace_more(limit=0)
                         for comment in submission.comments.list():
-                            cmt_data = {"body":comment.body,"score":comment.score,"depth":comment.depth, "author":str(comment.author), "parent":comment.parent_id}
+                            cmt_data = {"time": comment.created_utc, "body":comment.body,"score":comment.score,"depth":comment.depth, "author":str(comment.author), "parent":comment.parent_id}
                             cmts_data["t1_"+str(comment.id)] = cmt_data
                         data_dict["comments"] = cmts_data
 
